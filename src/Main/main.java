d4c1793a35dedfcadd36904441cd1d46605877b7
Keiya Honda <e195721@ie.u-ryukyu.ac.jp>
2019-12-02 18:34:52 +0900

    2nd commit

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,6 +1,11 @@
 public class main {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
