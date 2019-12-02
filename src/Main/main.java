d58bd954410659dc17439656f490c611bc56a44b
Keiya Honda <e195721@ie.u-ryukyu.ac.jp>
2019-12-02 19:11:12 +0900

    3rd commit

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,11 +1,10 @@
 public class main {
     public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("例外が発生しました。");
-            System.out.println(e.getMessage());
+        try{
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        } catch (NumberFormatException e){
+            System.out.println("例外っぽいわ〜");
         }
     }
 }
